fa78aadbc368e114909c5ae898d95329824e16fc
Vaisakh Vijayan <evvaisakh4@gmail.com>
2024-05-25 19:08:30 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AddEmpComponent } from './add-emp/add-emp.component';
+import { EditEmpComponent } from './edit-emp/edit-emp.component';
+import { EmpListComponent } from './emp-list/emp-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddEmpComponent,
+    EditEmpComponent,
+    EmpListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
